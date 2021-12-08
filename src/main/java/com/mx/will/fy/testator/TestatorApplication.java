74c3d3e664ebe128464ba969c8ca7aea5446db54
Luis Eduardo Bolivar Cardenas <theluisbolivar@Luiss-MacBook-Pro.local>
2021-12-08 11:40:08 -0600

    starting testator

---
+++ src/main/java/com/mx/will/fy/testator/TestatorApplication.java
@@ -0,0 +1,13 @@
+package com.mx.will.fy.testator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/will/fy/testator/TestatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.will.fy.testator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
